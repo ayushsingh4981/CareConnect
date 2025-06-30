100ecb40b6ca06094039e8af454e4794c62383f5
Ayush <155170758+ayushsingh4981@users.noreply.github.com>
2025-07-01 01:03:29 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,7 @@ const App: React.FC = () => {
   return (
     <BrowserRouter>
       <div className="App">
+      <img src="/black_circle_360x360.svg" classname="absolute fixed top-0 right-1 h-34" />
         <Routes>
           <Route 
             path="/login" 
